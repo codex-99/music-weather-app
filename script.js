2b0edf50831502c52a58bc8b6282a92593611fec
Yash Soni <thisisyashsoni.1011@gmail.com>
2022-01-18 14:28:13 +0530

    Integrated music in weather app

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ setInterval(() => {
 
 }, 1000);
 
+
 getWeatherData()
 function getWeatherData () {
     navigator.geolocation.getCurrentPosition((success) => {
@@ -38,11 +39,27 @@ function getWeatherData () {
 
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
         console.log(data)
+        const { current: { weather = [] } } = data || {};
+            if(weather && weather.length) {
+                console.log(weather[0])
+                playSong(weather[0].main)
+            }
         showWeatherData(data);
         })
 
+    }, (error) => {
+        let latitude = 28.704060, longitude  = 77.102493;
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+            const { current: { weather = [] } } = data || {};
+            if(weather && weather.length) {
+                console.log(weather[0])
+                playSong(weather[0].main)
+            }
+            showWeatherData(data);
+        })
     })
 }
+
 
 function showWeatherData (data){
     let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
@@ -101,19 +118,178 @@ function showWeatherData (data){
     weatherForecastEl.innerHTML = otherDayForcast;
 }
 
+
 // Adding genres according to humidity.
 function getGenre(data) {
-  let humidity = data.current;
-  console.log(`Humidity: ${humidity}`)
-  if (humidity >= 0 && humidity <= 30) {
-    return "emotional";
-  } else if (humidity >= 40 && humidity <= 70) {
-    return "bollywood";
+  
+  if (data === 'fog') {
+    return "Emotional";
+  } else if (data === '') {
+    return "Bollywood";
   } else {
-    return "pop";
+    return "Pop";
   }
 }
 
 
-
 // Music.js
+
+const music = document.querySelector('audio');
+const play = document.getElementById('play');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const genre = document.getElementById('genre');
+
+let progress = document.getElementById('progress')
+let total_duration = document.getElementById('duration')
+let current_time = document.getElementById('currentTime')
+const progress_div = document.getElementById('progress_div')
+
+const songList = [
+    {
+        name: "audio1",
+        title: "SiyaRam Lofi remix",
+        artist: "Unknown",
+        genre: "Emotional"
+    },{
+        name: "audio2",
+        title: "Lovely",
+        artist: "Billie Eilish",
+        genre: "Pop"
+    },{
+        name: "audio3",
+        title: "Industry Baby",
+        artist: "lil nas x",
+        genre: "Pop"
+    },{
+        name: "audio4",
+        title: "Dilli ki Ladki",
+        artist: "Tanzeel Khan",
+        genre: "Pop"
+    },{
+        name: "audio5",
+        title: "Kabhi Tumhhe",
+        artist: "Sony Music India",
+        genre: "Bollywood"
+    },{
+        name: "audio6",
+        title: "Kya tujhe abb",
+        artist: "T-Series",
+        genre: "Emotional"
+    },{
+        name: "audio7",
+        title: "Peed X Mood Swings",
+        artist: "One Touch Solutions",
+        genre: "Pop"
+    },{
+        name: "audio8",
+        title: "Bijlee Bijlee",
+        artist: "SuperHit songs",
+        genre: "Bollywood"
+    },{
+        name: "audio9",
+        title: "Manika Mage Hithe",
+        artist: "Angulia Entertainment",
+        genre: "Pop"
+    },{
+        name: "audio10",
+        title: "Aye Khuda",
+        artist: "T-Series",
+        genre: "Emotional"
+    },{
+        name: "audio11",
+        title: "Phone",
+        artist: "Mickey Singh",
+        genre: "Pop"
+    },
+]
+
+function playSong(weather){
+    var Genre = getGenre(weather);
+    let songs = [];
+    for(var i=0;i<songList.length;i++){
+        var obj = songList[i];
+        if(obj.genre === Genre){
+            songs.push(obj);
+        }
+    }
+
+    let isPlay = false;
+
+    const playMusic = ()=>{
+        isPlay = true;
+        music.play();
+        play.classList.replace('fa-play', 'fa-pause');
+        play.title = 'Pause';
+    }
+
+    const pauseMusic = ()=>{
+        isPlay = false;
+        music.pause();
+        play.classList.replace('fa-pause', 'fa-play');
+        play.title = 'Play';
+    }
+
+    play.addEventListener('click', ()=>{
+        isPlay? pauseMusic() : playMusic();
+    })
+
+    const loadSong = (songs)=>{
+    
+        title.textContent = songs.title;
+        artist.textContent = songs.artist;
+        genre.textContent = songs.genre;
+        music.src = `music/${songs.name}.mp3`;
+
+    }
+
+    let songIndex = 1;
+
+    const nextSong = ()=>{
+        songIndex = (songIndex + 1) % songs.length;
+        loadSong(songs[songIndex]);
+        playMusic();
+    }
+
+    const prevSong = ()=>{
+        songIndex = (songIndex - 1 + songs.length) % songs.length;
+        loadSong(songs[songIndex]);
+        playMusic();
+    }
+    loadSong(songs[0])
+
+    //progress bar
+
+    music.addEventListener('timeupdate', (event) => {
+        const {currentTime, duration} = event.srcElement;
+        let progress_time = (currentTime/duration) * 100;
+        progress.style.width = `${progress_time}%`;
+        let min = Math.floor(duration / 60);
+        let sec = Math.floor(duration % 60);
+        if(duration){
+            if(sec<10){
+                total_duration.textContent = `${min}:0${sec}`;
+            }else{
+                total_duration.textContent = `${min}:${sec}`;
+            }
+        }
+
+        let m = Math.floor(currentTime/60);
+        let s = Math.floor(currentTime%60);
+
+        if(currentTime){
+            if(s<10){
+                current_time.textContent = `${m}:0${s}`;
+            }else{
+                current_time.textContent = `${m}:${s}`;
+            }
+        }
+    })
+
+    music.addEventListener("ended", nextSong);
+    next.addEventListener('click', nextSong);
+    prev.addEventListener('click', prevSong);   
+
+}
